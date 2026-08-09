ca7bb68d626f5d665484e85d8d091c48abf3ac2c
mapi <rfriends2017@ymail.ne.jp>
2026-08-09 12:22:14 +0900

    Add files via upload

---
+++ lighttpd_install.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# =========================================
+# lighttpd
+#
+# 1.0 2025/01/08 new
+# 1.1 2026/08/09 cpu type
+ver=1.1
+# =========================================
+echo
+echo install lighttpd for mac osx Ver. $ver
+echo
+#
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "arm64" ]; then
+    # Apple Silicon
+    dir=/opt/homebrew
+    conf_dir=/opt/homebrew/etc/lighttpd
+elif [ "$ARCH" = "x86_64" ]; then
+    # Intel Mac
+    dir=/usr/local
+    conf_dir=/usr/local/etc/lighttpd
+else
+    echo "不明なアーキテクチャです: $ARCH"
+    exit
+fi
+
+
+user=`whoami`
+group=`groups $user | cut -d " " -f 1`
+home_dir=$HOME
+port=8000
+
+html_dir=$home_dir/rfriends3/script/html
+scr_dir=`pwd`
+
+brew update
+#brew upgrade
+brew install lighttpd
+#
+#mkdir -p $home_dir/lighttpd
+#mkdir -p $home_dir/lighttpd/sockets
+#mkdir -p $home_dir/lighttpd/uploads
+echo lighttpd > $home_dir/rfriends3/rfriends3_boot.txt
+
+mkdir -p $html_dir/temp
+ln -nfs $html_dir/temp $html_dir/webdav
+
+sudo cp -n $conf_dir/lighttpd.conf $conf_dir/lighttpd.conf.org
+#sudo cp -n $conf_dir/modules.conf $conf_dir/modules.conf.org
+#sudo cp -n $conf_dir/conf.d/fastcgi.conf $conf_dir/conf.d/fastcgi.conf.org
+#sudo cp -n $conf_dir/conf.d/webdav.conf $conf_dir/conf.d/webdav.conf.org
+
+sudo cp -p $scr_dir/lighttpd.conf.skel  $scr_dir/lighttpd.conf
+sed -i "" s%rfriendsdir%$dir%g $scr_dir/lighttpd.conf
+sed -i "" s%rfriendshomedir%$home_dir%g $scr_dir/lighttpd.conf
+sed -i "" s%rfriendsuser%$user%g        $scr_dir/lighttpd.conf
+sed -i "" s%rfriendsgroup%$group%g      $scr_dir/lighttpd.conf
+sed -i "" s%rfriendsport%$port%g      $scr_dir/lighttpd.conf
+sudo cp -p $scr_dir/lighttpd.conf $conf_dir/lighttpd.conf
+
+#sudo cp -p $scr_dir/modules.conf.skel $conf_dir/modules.conf
+#sudo cp -p $scr_dir/fastcgi.conf.skel $conf_dir/conf.d/fastcgi.conf
+#sudo cp -p $scr_dir/webdav.conf.skel  $conf_dir/conf.d/webdav.conf
+
+brew services restart lighttpd
